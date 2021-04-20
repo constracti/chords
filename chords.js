b2d9724c6976e0a58a4cee9501e370f3a50f19f0
constracti <constracti@gmail.com>
2021-04-21 00:24:16 +0300

    display interval form elements

--- chords.js
+++ chords.js
@@ -1,3 +1,4 @@
+// TODO translate terms
 function chords(options = null) {
 	if (options === null) {
 		options = '#chords';
@@ -8,6 +9,10 @@ function chords(options = null) {
 		};
 	}
 	const props = [
+		'dir',
+		'interval',
+		'primary',
+		'secondary',
 		'hide',
 		'text',
 		'larger',
@@ -25,12 +30,56 @@ function chords(options = null) {
 				if (typeof(options[prop]) === 'string')
 					options[prop] = options.form.find(options[prop]);
 			});
+			[
+				['up', 'up'],
+				['down', 'down'],
+			].forEach(x => {
+				options.dir.append('<option value="' + x[0] + '">' + x[1] + '</option>');
+			});
+			[
+				[0, '1st'],
+				[1, '2nd'],
+				[2, '3rd'],
+				[3, '4th'],
+				[4, '5th'],
+				[5, '6th'],
+				[6, '7th'],
+			].forEach(x => {
+				options.interval.append('<option value="' + x[0] + '">' + x[1] + '</option>');
+			});
+			[
+				[-1, 'diminished'],
+				[ 0, 'perfect'],
+				[ 1, 'augmented'],
+			].forEach(x => {
+				options.primary.append('<option value="' + x[0] + '">' + x[1] + '</option>');
+			});
+			[
+				[-2, 'diminished'],
+				[-1, 'minor'],
+				[ 0, 'major'],
+				[ 1, 'augmented'],
+			].forEach(x => {
+				options.secondary.append('<option value="' + x[0] + '">' + x[1] + '</option>');
+			});
 			options.hide.hide();
 			options.text.css('display', 'block')
 				.css('font-family', 'monospace')
 				.css('white-space', 'pre');
 			options.larger.hide();
 			options.smaller.hide();
+			options.interval.on('change', function() {
+				const is_primary = [0, 3, 4].includes(parseInt(options.interval.val()));
+				if (is_primary) {
+					options.primary.show();
+					options.secondary.hide();
+				} else {
+					options.primary.hide();
+					options.secondary.show();
+				}
+				options.primary.val('0');
+				options.secondary.val('0');
+			}).change();
 			options.form.on('submit', function() {
 				$.get(options.href).done(function(data) {
 					data = $('<div>' + data + '</div>').text();
